ec6f1893fd425a2c34e5712b42a174c20539beac
AnshikaS156 <saraswat.anshika156@gmail.com>
2025-08-11 16:50:40 +0530

    conflict

--- Fundamental-Practice-Problem/hello.java
+++ Fundamental-Practice-Problem/hello.java
@@ -1,6 +1,9 @@
 public class hello{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("This is a simple Java program.");
+   
     }
+
     
 }
